e7acf2615aab12b13743299937b7946b3b4b0a72
PSebastian96 <>
2023-08-21 19:14:39 +0000

    new tasks added

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -13,3 +13,7 @@ M.Datepicker.init(datepicker, {
 // select initialization
 let selects = document.querySelectorAll("select");
 M.FormSelect.init(selects);
+
+// collapsible initializataion
+let collapsibles = document.querySelectorAll(".collapsible");
+M.Collapsible.init(collapsibles);
